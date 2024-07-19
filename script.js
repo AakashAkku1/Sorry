0f4074939244a6e51749f72aa2ac30f5cfbd9efb
AakashKanaujiya1430 <164624759+AakashKanaujiya1430@users.noreply.github.com>
2024-07-19 17:22:16 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+var s = document.querySelector('#photo');
+function meri(){
+    
+    let content = document.querySelector('#photo');
+    let newChild = document.createElement('video');
+    newChild.src = "meri.mp4";
+    content.appendChild(newChild);
+
+    let content2 = document.querySelector('#text');
+    content2.innerHTML="Sorry Boldo aake Chup Chap se 😕";
+   
+}
+
+function Apki(){
+    
+    let content = document.querySelector('#photo');
+    let newChild = document.createElement('video');
+    newChild.src = "apki.mp4";
+    content.appendChild(newChild);
+
+    let content2 = document.querySelector('#text');
+    content2.innerHTML="Me nhi bol rha Sorry Hehehe 😍😎";
+   
+}
